fb24bd204fb0f9b16050f1c96b81340dd3019c34
Nicolas Wormser <nicolas.wormser@gmail.com>
2012-11-10 02:16:44 +0100

    UI update

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 gem 'jekyll'
 gem 'rake'
 gem 'sass'
+gem 'rdiscount'
+gem 'pygments.rb'
 # I'll need this into production but for now, let's focus onto what's Needed
 #gem 'uglifier'
 #gem 'therubyracer'
---
+++ _plugins/date_to_full_string.rb
@@ -0,0 +1,23 @@
+
+module Jekyll
+  module DateToFullStringFilter
+    def ordinalize(day)
+      if (11..13).include?(day % 100)
+        "#{day}th"
+      else
+        case day % 10
+        when 1; "#{day}<span class=\"ordinal\">st</span>"
+        when 2; "#{day}<span class=\"ordinal\">nd</span>"
+        when 3; "#{day}<span class=\"ordinal\">rd</span>"
+        else    "#{day}<span class=\"ordinal\">th</span>"
+        end
+      end
+    end
+    def date_to_full_string(date)
+      date.strftime("%B #{ordinalize(date.wday)}, %Y")
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::DateToFullStringFilter)
+
